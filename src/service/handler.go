50f75a98ac484712bacba8813f2d53d4e68f113a
imamdigmi <imam.digmi@gmail.com>
2017-05-27 06:06:47 +0700

    Add: json response

--- src/service/handler.go
+++ src/service/handler.go
@@ -1,11 +1,24 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+// Category struct manifest data
+type Category struct {
+	Name string
+	Product
+}
+
+// Product struct manifest data
+type Product struct {
+	Name    string
+	Version string
+}
 
 // GETIndex is root endpoint for get System info
 func GETIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -14,10 +27,40 @@ func GETIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 // GETCategory endpoint for get categories by Name
 func GETCategory(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Category, %s!\n", ps.ByName("category"))
+	w.Header().Set("Content-Type", "application/json")
+
+	category := &Category{
+		Name: ps.ByName("category"),
+	}
+
+	result, err := json.Marshal(category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(result)
+	return
 }
 
 // GETProduct endpoint for get product by name
 func GETProduct(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "%s with %s product\n", ps.ByName("category"), ps.ByName("product"))
+	w.Header().Set("Content-Type", "application/json")
+
+	product := &Category{
+		Name: ps.ByName("category"),
+		Product: Product{
+			Name:    ps.ByName("product"),
+			Version: "1.1.0",
+		},
+	}
+
+	result, err := json.Marshal(product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(result)
+	return
 }
