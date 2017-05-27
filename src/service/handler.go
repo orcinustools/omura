0f96f4dd3e5dc567aa57ba68386b44fb7355b8f7
imamdigmi <imam.digmi@gmail.com>
2017-05-28 06:35:07 +0700

    Update: orcinus file to json

--- src/service/handler.go
+++ src/service/handler.go
@@ -3,21 +3,44 @@ package service
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	yaml "gopkg.in/yaml.v2"
 )
 
 // Category struct manifest data
 type Category struct {
 	Name string
-	Product
+	OrcinusConf
 }
 
-// Product struct manifest data
-type Product struct {
-	Name    string
-	Version string
+// OrcinusConf struct manifest data
+type OrcinusConf struct {
+	Stack    string `json:"stack"`
+	Services struct {
+		Wordpress struct {
+			Image       string   `json:"image"`
+			Auth        bool     `json:"auth"`
+			Ports       []string `json:"ports"`
+			Environment []string `json:"environment"`
+		} `json:"wordpress"`
+	} `json:"services"`
+}
+
+func (c *OrcinusConf) getConf(category, product string) *OrcinusConf {
+	yamlFile, err := ioutil.ReadFile("./repository/" + category + "/" + product + "/orcinus.yml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	return c
 }
 
 // GETIndex is root endpoint for get System info
@@ -46,16 +69,10 @@ func GETCategory(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 // GETProduct endpoint for get product by name
 func GETProduct(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
+	var orcinusYaml OrcinusConf
+	orcinusYaml.getConf(ps.ByName("category"), ps.ByName("product"))
 
-	product := &Category{
-		Name: ps.ByName("category"),
-		Product: Product{
-			Name:    ps.ByName("product"),
-			Version: "1.1.0",
-		},
-	}
-
-	result, err := json.Marshal(product)
+	result, err := json.Marshal(orcinusYaml)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
