d25ec47b33283a9310e3e18314705540fa15378e
imamdigmi <imam.digmi@gmail.com>
2017-05-30 22:49:00 +0700

    Update: yaml handler

--- src/service/handler.go
+++ src/service/handler.go
@@ -12,28 +12,6 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-// Category struct manifest data
-type Category struct {
-	Name string
-}
-
-// Convert function to create dynamically json structure
-func convert(i interface{}) interface{} {
-	switch x := i.(type) {
-	case map[interface{}]interface{}:
-		m2 := map[string]interface{}{}
-		for k, v := range x {
-			m2[k.(string)] = convert(v)
-		}
-		return m2
-	case []interface{}:
-		for i, v := range x {
-			x[i] = convert(v)
-		}
-	}
-	return i
-}
-
 // GETIndex is root endpoint for get System info
 func GETIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
@@ -41,37 +19,24 @@ func GETIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 // GETCategory endpoint for get categories by Name
 func GETCategory(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Header().Set("Content-Type", "application/json")
-
-	category := &Category{
-		Name: ps.ByName("category"),
-	}
-
-	result, err := json.Marshal(category)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(result)
-	return
+	fmt.Fprint(w, "Your "+ps.ByName("category")+" has arrived!\n")
 }
 
 // GETProduct endpoint for get product by name
 func GETProduct(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
+	var data map[string]Services
 	yamlFile, err := ioutil.ReadFile(os.Getenv("GOPATH") + "/bin/repository/" + ps.ByName("category") + "/" + ps.ByName("product") + "/orcinus.yml")
 	if err != nil {
 		log.Printf("yamlFile.Get err   #%v ", err)
 	}
 
-	var body interface{}
-	if err := yaml.Unmarshal([]byte(yamlFile), &body); err != nil {
+	if err := yaml.Unmarshal([]byte(yamlFile), &data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	result, err := json.Marshal(convert(body))
+	result, err := json.Marshal(data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
--- src/service/init.go
+++ src/service/init.go
@@ -1,6 +1,54 @@
 package service
 
+import yaml "gopkg.in/yaml.v2"
+
 // Initialize for initiates service package
 func Initialize() (err error) {
 	return
+}
+
+// Services response struct
+type Services struct {
+	Service map[string]Stack
+}
+
+// Stack response struct
+type Stack struct {
+	Image       string   `json:"image"`
+	Auth        bool     `json:"auth"`
+	Ports       []string `json:"ports"`
+	Environment []string `json:"environment"`
+}
+
+// ProductManifest response struct
+type ProductManifest struct {
+	Name         string   `json:"name"`
+	Logo         string   `json:"logo"`
+	Dependencies []string `json:"dependencies"`
+}
+
+// ResponseFormat response struct
+type ResponseFormat struct {
+	Spec []struct {
+		Name     string `json:"name"`
+		Logo     string `json:"logo"`
+		Services struct {
+			Image       string   `json:"image"`
+			Auth        bool     `json:"auth"`
+			Ports       []string `json:"ports"`
+			Environment []string `json:"environment"`
+		} `json:"service"`
+	} `json:"spec"`
+}
+
+// UnmarshalYAML for handling dynamic Orcinus.yml
+func (e *Services) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var services map[string]Stack
+	if err := unmarshal(&services); err != nil {
+		if _, ok := err.(*yaml.TypeError); !ok {
+			return err
+		}
+	}
+	e.Service = services
+	return nil
 }
