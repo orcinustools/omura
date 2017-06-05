d121952ca550f103a66687b2a905ac6ab68df39a
anak10thn <anak10thn@gmail.com>
2017-06-05 18:14:19 +0700

    ADD:file serving

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/orcinustools/omura/src/service"
 )
 
 func main() {
+	directory := os.Getenv("GOPATH") + "/bin/repository/"
 	router := httprouter.New()
-	router.GET("/", service.GETIndex)
-	router.GET("/:category", service.GETCategory)
-	router.GET("/:category/:product", service.GETProduct)
+	router.GET("/apis", service.GETIndex)
+	router.GET("/apis/:category", service.GETCategory)
+	router.GET("/apis/:category/:product", service.GETProduct)
+	router.ServeFiles("/statics/*filepath", http.Dir(directory))
+
+	log.Printf("Serving %s on HTTP port: %s\n", directory, ":8080")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
